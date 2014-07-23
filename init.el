ca53c2f3a5e55f3d0d84560c42d2fbc4deeea12d
Yasuyuki Ageishi <4ge15h1@gmail.com>
2014-07-24 08:09:32 +0900

    added guru-mode and melpa

--- init.el
+++ init.el
@@ -6,3 +6,14 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
+
+(add-to-list 'load-path "~/.emacs.d/vendor")
+(require 'guru-mode)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compativility Libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
