ce441920823d4d098ab6037da7e960ca408bb288
Yasuyuki Ageishi <age15h1@me.com>
2018-01-20 16:24:39 +0900

    added terraform, hcl

--- init.el
+++ init.el
@@ -86,17 +86,7 @@
     ("a240eea56be4be98f79db914603c44bbd33f46e0d4d1d14187f0206bfa4bfea3" "c8b3b9880a637c6585d78b586e23ad88f1c6a39a917038c1e2490a8c3c7f1d63" "b6ae47868dfb79ee2a1d3eee740aea0178d7fb1c6a8bb8eca9bba6292a07abbf" "168cd7977a4df0906c40b659267e1ef13cd2d8a125558d17f0151d4f8a8fce72" "33f420603a81175875e11f8d9c18eda4ce19d45462f7ff9fa4581c926ed88944" "42c235cdaccf1fd9bb40ddc087632efa3d90ae30ec27aa8490bde8e6ddde23ba" "1004c7f179d22a08aba6f8d3100b4762e4e9a51e69aa8fcf95139abe642c24db" "e2686ce582e23b34e3c275003a068e184937d1c957b39b41363ac73b5185433f" default)))
  '(package-selected-packages
    (quote
-    (go-mode yaml-mode web-mode sml-mode scss-mode scala-mode sass-mode org markdown-mode jsx-mode flycheck exec-path-from-shell clojure-cheatsheet 4clojure))))
-
-
-
-;; reference https://truongtx.me/2014/03/10/emacs-setup-jsx-mode-and-jsx-syntax-checking/#
-(add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
-(defadvice web-mode-highlight-part (around tweak-jsx activate)
-  (if (equal web-mode-content-type "jsx")
-      (let ((web-mode-enable-part-face nil))
-        ad-do-it)
-    ad-do-it))
+    (terraform-mode hcl-mode go-mode yaml-mode web-mode sml-mode scss-mode scala-mode sass-mode org markdown-mode jsx-mode flycheck exec-path-from-shell clojure-cheatsheet 4clojure))))
 
 (defun my-web-mode-hook ()
   (setq web-mode-markup-indent-offset 2)
@@ -107,6 +97,17 @@
   (set-face-attribute 'web-mode-html-attr-value-face nil :foreground "color-35")
   (set-face-attribute 'web-mode-html-attr-equal-face nil :foreground "brightwhite"))
 (add-hook 'web-mode-hook 'my-web-mode-hook)
+
+(setq web-mode-content-types-alist
+      '(("jsx" . "\\.js[x]?\\'")))
+
+;; reference https://truongtx.me/2014/03/10/emacs-setup-jsx-mode-and-jsx-syntax-checking/#
+(add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
 
 ;; (require 'jsx-mode)
 ;; (add-to-list 'auto-mode-alist '("\\.js$" . jsx-mode))
